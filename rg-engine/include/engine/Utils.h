276592e054aa0dbf60b146d0ad802808bd659131
spaske00 <spaskeasm@gmail.com>
2024-05-14 14:25:58 +0200

    Replace macros use with std::source_location

--- rg-engine/include/engine/Utils.h
+++ rg-engine/include/engine/Utils.h
@@ -1,23 +1,20 @@
 //
-// Created by oracle on 13.5.24..
+// Created by spaske00 on 13.5.24..
 //
 
 #ifndef MATF_RG_PROJECT_UTILS_H
 #define MATF_RG_PROJECT_UTILS_H
 
-#include<memory>
-
-#define RG_GUARANTEE(expr, msg) do { rg::utils::EngineError::guarantee(expr, #expr, msg, __FILE__, __LINE__); } while(0)
-#define RG_SHOULD_NOT_REACH_HERE(msg) do { rg::utils::EngineError::should_not_reach_here(msg, __FILE__, __LINE__); } while(0)
-#define RG_UNIMPLEMENTED(msg) do { rg::utils::EngineError::unimplemented(msg, __FILE__, __LINE__); } while(0)
+#include <source_location>
+#include <memory>
 
 namespace rg::utils {
 
     class EngineError {
     public:
-        static void guarantee(bool expr, std::string_view expr_str, std::string_view msg, std::string_view file, int line);
-        static void should_not_reach_here(std::string_view msg, std::string_view file, int line);
-        static void unimplemented(std::string_view msg, std::string_view file, int line);
+        static void guarantee(bool expr, std::string_view msg, std::source_location source_location = std::source_location::current());
+        static void should_not_reach_here(std::string_view msg, std::source_location source_location = std::source_location::current());
+        static void unimplemented(std::string_view msg, std::source_location source_location = std::source_location::current());
     };
 
 }
--- rg-engine/src/Utils.cpp
+++ rg-engine/src/Utils.cpp
@@ -1,26 +1,25 @@
 //
-// Created by oracle on 13.5.24..
+// Created by spaske00 on 13.5.24..
 //
 
 #include "engine/Utils.h"
 #include <spdlog/spdlog.h>
 
 namespace rg::utils {
-    void EngineError::guarantee(bool expr, std::string_view expr_str, std::string_view msg, std::string_view file,
-                                int line) {
+    void EngineError::guarantee(bool expr, std::string_view msg, std::source_location source_location) {
         if (!expr) {
-           spdlog::error("RG_GUARANTEE ({:}) failed in file:{:}:{:}. {:}", expr_str, file, line, msg);
+           spdlog::error("RG_GUARANTEE failed in file:{:}:{:}. {:}", source_location.file_name(), source_location.line(), msg);
            exit(EXIT_FAILURE);
         }
     }
 
-    void EngineError::should_not_reach_here(std::string_view msg, std::string_view file, int line) {
-        spdlog::error("RG_SHOULD_NOT_REACH_HERE in file:{:}:{:}. {:}", file, line, msg);
+    void EngineError::should_not_reach_here(std::string_view msg, std::source_location source_location) {
+        spdlog::error("RG_SHOULD_NOT_REACH_HERE in file:{:}:{:}. {:}", source_location.file_name(), source_location.line(), msg);
         exit(EXIT_FAILURE);
     }
 
-    void EngineError::unimplemented(std::string_view msg, std::string_view file, int line) {
-        spdlog::error("RG_UNIMPLEMENTED in file:{:}:{:}. {:}", file, line, msg);
+    void EngineError::unimplemented(std::string_view msg, std::source_location source_location) {
+        spdlog::error("RG_UNIMPLEMENTED in file:{:}:{:}. {:}", source_location.file_name(), source_location.line(), msg);
         exit(EXIT_FAILURE);
     }
 }
