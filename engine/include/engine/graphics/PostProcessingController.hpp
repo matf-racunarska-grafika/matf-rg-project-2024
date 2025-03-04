e2d8ebd0a41c353faf230020bb0282220f3a4398
viktor(mi22091) <viktorsevic@icloud.com>
2025-03-04 23:29:30 +0100

    added comments to PostProcessingController.hpp

--- engine/include/engine/graphics/PostProcessingController.hpp
+++ engine/include/engine/graphics/PostProcessingController.hpp
@@ -1,3 +1,7 @@
+/**
+* @file PostProcessingController.hpp
+* @brief Defines the PostProcessingController class that manages resources and rendering for bloom and filter effects.
+*/
 #ifndef POSTPROCESSINGCONTROLLER_HPP
 #define POSTPROCESSINGCONTROLLER_HPP
 
@@ -15,27 +19,79 @@ namespace engine::graphics {
     std::string m_active_filter = "noFilter";
 
   public:
+    /**
+     * @brief calls all the functions to prepare resources for rendering bloom and filters
+     */
+    void initialize() override;
+
+    /**
+     * @brief prepares the final framebuffer that will be rendered to the screen
+     * @param wHeight height of the screen
+     * @param wWidth width of the screen
+     */
+    void prepare_filter_effect(unsigned int wHeight, unsigned int wWidth);
+
+    /**
+     * @brief sets up the hdr framebuffer and color and pingpong buffers for bloom
+     * @param wHeight height of the screen
+     * @param wWidth width of the screen
+     */
+    void prepare_bloom_effect(unsigned int wHeight, unsigned int wWidth);
+
+    /**
+     * @brief sets up uniforms for default bloom shaders
+     */
+    void prepare_bloom_shaders();
+
+    /**
+     * @brief a getter for the name of the active filter
+     * @return the name of the active filter shader
+     */
+    std::string get_active_filter() const;
+
+    /**
+     * @brief a setter for the name of the active filter
+     * @param filterShader the name of the filter shader that should be activated
+     */
+    void set_active_filter(std::string filterShader);
+
+    /**
+     * @brief draws all post-processing effects into framebuffers
+     */
+    void draw() override;
+
+    /**
+     * @brief binds the appropriate framebuffer and clears buffers
+     */
+    void begin_draw() override;
+
+    /**
+     * @brief unbinds the framebuffer and swaps buffers
+     */
+    void end_draw() override;
+
+    /**
+     * @brief applies the bloom effect
+     */
+    void draw_bloom();
+
+    /**
+     * @brief applies the current active filter
+     */
+    void draw_filter();
+
+    /**
+     * @brief deletes all framebuffers and related textures
+     */
+    void terminate() override;
+
+    /**
+     * @brief helps to easily identify the controller in logs by giving it a name
+     * @return name of the Controller class
+     */
     std::string_view name() const override {
       return "PostProcessingController";
     }
-    void initialize() override;
-
-    void prepare_filter_effect(unsigned int wHeight, unsigned int wWidth);
-    void prepare_bloom_effect(unsigned int wHeight, unsigned int wWidth);
-    void prepare_bloom_shaders();
-
-    std::string get_active_filter() const;
-    void set_active_filter(std::string filterShader);
-
-    void draw() override;
-
-    void begin_draw() override;
-    void end_draw() override;
-
-    void draw_bloom();
-    void draw_filter();
-
-    void terminate() override;
   };
 }
 
