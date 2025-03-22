4edbdd30af0bf0f7c2637bf80afb451e87e47545
Aleksa Vukadinović <alexvukadin8@gmail.com>
2025-03-22 16:17:01 +0100

    BloomController

---
+++ engine/include/engine/graphics/BloomController.hpp
@@ -0,0 +1,15 @@
+#ifndef BLOOMCONTROLLER_HPP
+#define BLOOMCONTROLLER_HPP
+
+#include <engine/graphics/Camera.hpp>
+#include <engine/core/Controller.hpp>
+#include <engine/platform/PlatformEventObserver.hpp>
+
+class BloomController final : public engine::core::Controller {
+public:
+
+private:
+};
+
+#endif //BLOOMCONTROLLER_HPP
+
