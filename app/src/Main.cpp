24a8843545916606f5e4e60699d5a560ceb9acd9
Aleksa Vukadinović <alexvukadin8@gmail.com>
2025-04-06 20:04:50 +0200

    Fixed compiler warning

--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -6,6 +6,6 @@
  * Start here...
  */
 int main(int argc, char** argv) {
-    auto app = std::make_unique<app::MyApp>();
+    const auto app = std::make_unique<app::MyApp>();
     return app->run(argc, argv);
 }
