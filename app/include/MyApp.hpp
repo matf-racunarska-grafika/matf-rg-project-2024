a6f768ce5ecb8f7a9cce9ee20a59a1c4e47b29e5
Ana <ana.vidakovic03@gmail.com>
2025-05-13 13:05:16 +0200

    MyApp setup

---
+++ app/include/MyApp.hpp
@@ -0,0 +1,17 @@
+//
+// Created by ana on 5/13/25.
+//
+
+#ifndef MYAPP_HPP
+#define MYAPP_HPP
+#include <engine/core/App.hpp>
+
+namespace app {
+
+class MyApp : public engine::core::App {
+    void app_setup() override;
+};
+
+}// app
+
+#endif //MYAPP_HPP
---
+++ app/src/MyApp.cpp
@@ -0,0 +1,11 @@
+//
+// Created by ana on 5/13/25.
+//
+
+#include "../include/MyApp.hpp"
+
+#include <spdlog/spdlog.h>
+
+namespace app {
+void MyApp::app_setup() { spdlog::info("App setup completed!"); }
+}// app
