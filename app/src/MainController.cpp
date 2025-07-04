01bc62d85e4faa744ef469e7277b6f56535bf5a4
teodoramaksimovic <teamax18@gmail.com>
2025-07-04 02:04:59 +0200

    Create base for MainController

---
+++ app/src/MainController.cpp
@@ -0,0 +1,16 @@
+//
+// Created by teodora on 4.7.25..
+//
+
+#include "engine/platform/PlatformController.hpp"
+#include "spdlog/spdlog.h"
+#include <MainController.hpp>
+
+
+namespace app {
+    void MainController::initialize() {
+        spdlog::info("MainController initialized!");
+
+    }
+
+} // app
--- app/src/MyApp.cpp
+++ app/src/MyApp.cpp
@@ -2,6 +2,9 @@
 // Created by teodora on 4.7.25..
 //
 
+#include "MainController.hpp"
+
+
 #include <MyApp.hpp>
 
 #include <spdlog/spdlog.h>
@@ -9,5 +12,7 @@
 namespace app {
     void MyApp::app_setup() {
         spdlog::info("App setup completed!");
+        auto main_controller = register_controller<app::MainController>();
+        main_controller->after(engine::core::Controller::get<engine::core::EngineControllersEnd>());
     }
 } // app
