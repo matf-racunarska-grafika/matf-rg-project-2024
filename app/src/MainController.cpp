ecdb6e7527c798c69893c3c1a8888d55308e1aed
Ognjen Marković <ognjen.mark03@gmail.com>
2025-07-14 20:40:32 +0200

    Add ESC functionality

--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -3,12 +3,15 @@
 //
 
 #include <MainController.hpp>
+#include <engine/platform/PlatformController.hpp>
 #include <spdlog/spdlog.h>
 
 namespace app {
-void MainController::initialize() {
-    spdlog::info("MainController initialized");
+void MainController::initialize() { spdlog::info("MainController initialized"); }
 
-
+bool MainController::loop() {
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+    if (platform->key(engine::platform::KeyId::KEY_ESCAPE).is_down()) { return false; }
+    return true;
 }
 }// app
