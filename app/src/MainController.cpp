3cdff92bf575d4cacb69b0b85708f964f5d97495
Ognjen Marković <ognjen.mark03@gmail.com>
2025-07-14 21:42:43 +0200

    Spend 30 minutes on finding line separator bug

--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -4,14 +4,29 @@
 
 #include <MainController.hpp>
 #include <engine/platform/PlatformController.hpp>
+#include <engine/resources/ResourcesController.hpp>
 #include <spdlog/spdlog.h>
 
 namespace app {
-void MainController::initialize() { spdlog::info("MainController initialized"); }
+    void MainController::initialize() { spdlog::info("MainController initialized"); }
 
-bool MainController::loop() {
-    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
-    if (platform->key(engine::platform::KeyId::KEY_ESCAPE).is_down()) { return false; }
-    return true;
-}
-}// app
+    bool MainController::loop() {
+        auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+        if (platform->key(engine::platform::KeyId::KEY_ESCAPE).is_down()) { return false; }
+        return true;
+    }
+
+    void MainController::draw_backpack() {
+        //Model
+        auto resources = engine::core::Controller::get<engine::resources::ResourcesController>();
+        engine::resources::Model* model = resources->model("backpack");
+        //Shader
+        engine::resources::Shader* shader = resources->shader("basic");
+
+        model->draw(shader);
+    }
+
+    void MainController::draw() {
+        draw_backpack();
+    }
+    }// app
