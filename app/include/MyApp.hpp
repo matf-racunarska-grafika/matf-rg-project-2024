a45d704213a3fe6ca5b1d8f42ae6e7a6fb049c48
cvnpko <lazar.igob@gmail.com>
2025-07-14 20:46:50 +0200

    feat: create empty MyApp and app_setup

---
+++ app/include/MyApp.hpp
@@ -0,0 +1,17 @@
+//
+// Created by cvnpko on 7/14/25.
+//
+
+#ifndef MYAPP_HPP
+#define MYAPP_HPP
+#include <engine/core/App.hpp>
+
+namespace app {
+
+class MyApp final : public engine::core::App {
+    void app_setup() override;
+};
+
+}// app
+
+#endif //MYAPP_HPP
---
+++ app/src/MyApp.cpp
@@ -0,0 +1,11 @@
+//
+// Created by cvnpko on 7/14/25.
+//
+
+#include "../include/MyApp.hpp"
+
+#include <spdlog/spdlog.h>
+
+namespace app {
+void MyApp::app_setup() { spdlog::info("MyApp::app_setup"); }
+}// app
