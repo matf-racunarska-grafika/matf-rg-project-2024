268d4c0203fb8bfa4532bb8ec25cb8d5bf88f3d3
Ognjen Marković <ognjen.mark03@gmail.com>
2025-07-15 20:15:20 +0200

    Add GUI

--- app/src/MyApp.cpp
+++ app/src/MyApp.cpp
@@ -2,6 +2,7 @@
 // Created by ognje on 7/14/2025.
 //
 
+#include <GuiController.hpp>
 #include <MainController.hpp>
 #include <MyApp.hpp>
 
@@ -11,6 +12,8 @@ namespace app {
     void MyApp::app_setup() {
         spdlog::info("App setup completed");
         auto main_controller = register_controller<app::MainController>();
+        auto gui_controller = register_controller<GUIController>();
         main_controller->after(engine::core::Controller::get<engine::core::EngineControllersEnd>());
+        main_controller->before(gui_controller);
     }
 }// app
