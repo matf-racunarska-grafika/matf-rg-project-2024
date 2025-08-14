dfe5282c440d3195e6a6647982a8840f148d66c8
aleksafilipovic <aleksafilipovic2003@gmail.com>
2025-08-14 19:15:51 +0200

    Add models for blue and red players. Add ball, grass field, goalkeeper and reflectors. Implement instancing

--- app/src/GuiController.cpp
+++ app/src/GuiController.cpp
@@ -24,7 +24,7 @@ void GUIController::draw() {
     graphics->begin_gui();
     const auto &c = *camera;
 
-    auto goal = engine::core::Controller::get<engine::resources::ResourcesController>()->model("goal");
+    auto goal = engine::core::Controller::get<engine::resources::ResourcesController>()->model("ball");
     static float f = 0.0f;
     ImGui::Begin("INFO");
 
@@ -42,15 +42,103 @@ void GUIController::draw() {
                                                        .x, c.Front
                                                             .y, c.Front
                                                                  .z);
+            ImGui::Text("Worldup : (%f, %f, %f)", c.WorldUp.x, c.WorldUp.y, c.WorldUp.z);
             ImGui::EndTabItem();
         }
 
-        if (ImGui::BeginTabItem("GOAL")) {
+        if (ImGui::BeginTabItem("teren")) {
+            // ImGui::Text("Loaded from: %s", goal->path().c_str());
+            ImGui::DragFloat("Teren scale", &mainController->trava_skalirano, 0.005, 0.0, 10.0);
+            ImGui::DragFloat("Teren x", &mainController->trava_x, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Teren y", &mainController->trava_y, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Teren z", &mainController->trava_z, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Ugao trava", &mainController->ugao_trava, 0.50, -100.0, 100.0);
+            ImGui::DragFloat("Vektor x", &mainController->osa_x, 0.005, -1.0f, 1.0f);
+            ImGui::DragFloat("Vektor y", &mainController->osa_y, 0.005, -1.0f, 1.0f);
+            ImGui::DragFloat("Vektor z", &mainController->osa_z, 0.005, -1.0f, 1.0f);
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("lopta")) {
+            // ImGui::Text("Loaded from: %s", goal->path().c_str());
+            ImGui::DragFloat("Lopta scale", &mainController->lopta_skalirano, 0.005, 0.0, 10.0);
+            ImGui::DragFloat("Lopta x", &mainController->lopta_x, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Lopta y", &mainController->lopta_y, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Lopta z", &mainController->lopta_z, 0.005, -100.0, 100.0);
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("Gol")) {
             ImGui::Text("Loaded from: %s", goal->path().c_str());
-            ImGui::DragFloat("football_goal scale", &mainController->goal_scale, 0.035, 0.0, 5.0);
+            ImGui::DragFloat("Goal scale", &mainController->gol_skalirano, 0.005, 0.0, 10.0);
+            ImGui::DragFloat("Goal x", &mainController->gol_x, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Goal y", &mainController->gol_y, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Goal z", &mainController->gol_z, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Ugao trava", &mainController->ugao_gol, 0.02, -100.0, 100.0);
+            ImGui::DragFloat("Vektor x", &mainController->gosa_x, 0.005, -1.0f, 1.0f);
+            ImGui::DragFloat("Vektor y", &mainController->gosa_y, 0.005, -1.0f, 1.0f);
+            ImGui::DragFloat("Vektor z", &mainController->gosa_z, 0.005, -1.0f, 1.0f);
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("Refletkor1")) {
+            ImGui::Text("Loaded from: %s", goal->path().c_str());
+            ImGui::DragFloat("Goal scale", &mainController->ref_skalirano, 0.003, 0.0, 10.0);
+            ImGui::DragFloat("Goal x", &mainController->ref_x, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Goal y", &mainController->ref_y, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Goal z", &mainController->ref_z, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Ugao trava", &mainController->ref1_ugao, 0.02, -100.0, 100.0);
+            ImGui::DragFloat("Vektor x", &mainController->ref_xv, 0.005, -1.0f, 1.0f);
+            ImGui::DragFloat("Vektor y", &mainController->ref_yv, 0.005, -1.0f, 1.0f);
+            ImGui::DragFloat("Vektor z", &mainController->ref_zv, 0.005, -1.0f, 1.0f);
             ImGui::EndTabItem();
         }
 
+        if (ImGui::BeginTabItem("Refletkor2")) {
+            ImGui::Text("Loaded from: %s", goal->path().c_str());
+            ImGui::DragFloat("Goal scale", &mainController->ref_skalirano, 0.003, 0.0, 10.0);
+            ImGui::DragFloat("Goal x", &mainController->ref2_x, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Goal y", &mainController->ref2_y, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Goal z", &mainController->ref2_z, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Ugao trava", &mainController->ref2_ugao, 1.0, -180.0, 180.0);
+            ImGui::DragFloat("Vektor x", &mainController->ref2_xv, 0.005, -5.0f, 5.0f);
+            ImGui::DragFloat("Vektor y", &mainController->ref2_yv, 0.005, -5.0f, 5.0f);
+            ImGui::DragFloat("Vektor z", &mainController->ref2_zv, 0.005, -5.0f, 5.0f);
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("Crveni")) {
+            // ImGui::Text("Loaded from: %s", goal->path().c_str());
+            ImGui::DragFloat("Teren scale", &mainController->crveni_skalirano, 0.005, 0.0, 10.0);
+            ImGui::DragFloat("Teren x", &mainController->crveni_x, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Teren y", &mainController->crveni_y, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Teren z", &mainController->crveni_z, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Ugao crveni", &mainController->ugao_crveni, 0.50, -100.0, 100.0);
+            ImGui::DragFloat("Vektor x", &mainController->crveni_xv, 0.005, -1.0f, 1.0f);
+            ImGui::DragFloat("Vektor y", &mainController->crveni_yv, 0.005, -1.0f, 1.0f);
+            ImGui::DragFloat("Vektor z", &mainController->crveni_zv, 0.005, -1.0f, 1.0f);
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("plavi")) {
+            // ImGui::Text("Loaded from: %s", goal->path().c_str());
+            ImGui::DragFloat("Teren scale", &mainController->plavi_skalirano, 0.005, 0.0, 10.0);
+            ImGui::DragFloat("Teren x", &mainController->plavi_x, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Teren y", &mainController->plavi_y, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Teren z", &mainController->plavi_z, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Ugao plavi", &mainController->ugao_plavi, 0.50, -180.0, 180.0);
+            ImGui::DragFloat("Vektor x", &mainController->plavi_xv, 0.005, -1.0f, 1.0f);
+            ImGui::DragFloat("Vektor y", &mainController->plavi_yv, 0.005, -1.0f, 1.0f);
+            ImGui::DragFloat("Vektor z", &mainController->plavi_zv, 0.005, -1.0f, 1.0f);
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("golman")) {
+            // ImGui::Text("Loaded from: %s", goal->path().c_str());
+            ImGui::DragFloat("Teren scale", &mainController->golman_skalirano, 0.005, 0.0, 10.0);
+            ImGui::DragFloat("Teren x", &mainController->golman_x, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Teren y", &mainController->golman_y, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Teren z", &mainController->golman_z, 0.005, -100.0, 100.0);
+            ImGui::DragFloat("Ugao golman", &mainController->ugao_golman, 0.50, -180.0, 180.0);
+            ImGui::DragFloat("Vektor x", &mainController->golman_xv, 0.005, -1.0f, 1.0f);
+            ImGui::DragFloat("Vektor y", &mainController->golman_yv, 0.005, -1.0f, 1.0f);
+            ImGui::DragFloat("Vektor z", &mainController->golman_zv, 0.005, -1.0f, 1.0f);
+            ImGui::EndTabItem();
+        }
         ImGui::EndTabBar();
     }
 
--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -33,48 +33,23 @@ void MainController::initialize() {
     auto observer = std::make_unique<MainPlatformEventObserver>();
     engine::core::Controller::get<engine::platform::PlatformController>()->register_platform_event_observer(
             std::move(observer));
+
+    auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+    auto camera = graphics->camera();
+
+    camera->Yaw = -90.0f;
+    camera->Pitch = -5.79f;
+    camera->rotate_camera(0, 0);
+    camera->Position = glm::vec3(1.0f, 7.6f, 13.3f);
+    // camera->rotate_camera(glm::radians(-90.0f), glm::radians(-5.7f));
+
+
 };
 
 bool MainController::loop() {
     auto platform = get<engine::platform::PlatformController>();
     if (platform->key(engine::platform::KEY_ESCAPE).is_down()) { return false; }
     return true;
-}
-
-void MainController::draw_basketball() {
-    //  Model
-    auto resources = get<engine::resources::ResourcesController>();
-    auto graphics = get<engine::graphics::GraphicsController>();
-
-    engine::resources::Model *cat = resources->model("basketball");
-    //  Shader
-    engine::resources::Shader *shader = resources->shader("basic");
-
-    shader->use();
-
-    shader->set_mat4("projection", graphics->projection_matrix());
-    shader->set_mat4("view", graphics->camera()->view_matrix());
-
-    glm::mat4 model = glm::mat4(1.0f);
-    model = glm::translate(model, glm::vec3(0.0f, 0.0f, 5.0f));
-
-    model = glm::scale(model, glm::vec3(0.25f));
-    shader->set_mat4("model", model);
-    cat->draw(shader);
-}
-
-void MainController::begin_draw() { engine::graphics::OpenGL::clear_buffers(); }
-
-void MainController::end_draw() {
-    auto platform = get<engine::platform::PlatformController>();
-    platform->swap_buffers();
-}
-
-void MainController::draw() {
-
-    draw_football_pitch();
-    //draw_basketball();
-    draw_skybox();
 }
 
 void MainController::update_camera() {
@@ -103,16 +78,40 @@ void MainController::draw_skybox() {
     auto skybox = resources->skybox("field_skybox");
     auto shader = resources->shader("skybox");
     auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
-
     graphics->draw_skybox(shader, skybox);
 }
 
-void MainController::draw_football_pitch() {
+void MainController::draw_goal() {
     //  Model
     auto resources = get<engine::resources::ResourcesController>();
     auto graphics = get<engine::graphics::GraphicsController>();
 
-    engine::resources::Model *cat = resources->model("goal");
+    engine::resources::Model *goal = resources->model("goal");
+    engine::resources::Shader *shader = resources->shader("goalShader");
+
+    shader->use();
+    shader->set_vec3("objectColor", glm::vec3(1.0f, 1.0f, 1.0f));
+    shader->set_vec3("lightPos", glm::vec3(0.0f, 10.0f, 0.0f));
+    shader->set_vec3("viewPos", graphics->camera()->Position);
+    shader->set_mat4("projection", graphics->projection_matrix());
+    shader->set_mat4("view", graphics->camera()->view_matrix());
+
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(gol_x, gol_y, gol_z));
+    model = glm::rotate(model, glm::radians(ugao_gol), glm::vec3(gosa_x, gosa_y, gosa_z));
+    model = glm::scale(model, glm::vec3(gol_skalirano));
+    shader->set_mat4("model", model);
+
+    goal->draw(shader);
+
+}
+
+void MainController::draw_grass() {
+    //  Model
+    auto resources = get<engine::resources::ResourcesController>();
+    auto graphics = get<engine::graphics::GraphicsController>();
+
+    engine::resources::Model *grass = resources->model("grass");
     //  Shader
     engine::resources::Shader *shader = resources->shader("basic");
 
@@ -122,12 +121,169 @@ void MainController::draw_football_pitch() {
     shader->set_mat4("view", graphics->camera()->view_matrix());
 
     glm::mat4 model = glm::mat4(1.0f);
-    model = glm::translate(model, glm::vec3(0.0f, 0.0f, 5.0f));
-
-    model = glm::scale(model, glm::vec3(goal_scale));
+    model = glm::translate(model, glm::vec3(trava_x, trava_y, trava_z));
+    model = glm::rotate(model, glm::radians(ugao_trava), glm::vec3(osa_x, osa_y, osa_z));
+    model = glm::scale(model, glm::vec3(trava_skalirano));
     shader->set_mat4("model", model);
-    cat->draw(shader);
+    grass->draw(shader);
 }
+
+void MainController::draw_ball() {
+    //  Model
+    auto resources = get<engine::resources::ResourcesController>();
+    auto graphics = get<engine::graphics::GraphicsController>();
+
+    engine::resources::Model *ball = resources->model("ball");
+    //  Shader
+    engine::resources::Shader *shader = resources->shader("basic");
+
+    shader->use();
+
+    shader->set_mat4("projection", graphics->projection_matrix());
+    shader->set_mat4("view", graphics->camera()->view_matrix());
+
+    glm::mat4 model = glm::mat4(1.0f);
+    // namesti y
+    model = glm::translate(model, glm::vec3(lopta_x, lopta_y, lopta_z));
+
+    model = glm::scale(model, glm::vec3(lopta_skalirano));
+    shader->set_mat4("model", model);
+    ball->draw(shader);
+
+};
+
+void MainController::begin_draw() { engine::graphics::OpenGL::clear_buffers(); }
+
+void MainController::end_draw() {
+    auto platform = get<engine::platform::PlatformController>();
+    platform->swap_buffers();
+}
+
+void MainController::draw() {
+    draw_grass();
+    draw_reflectors();
+    draw_goalkeeper();
+    draw_red_players();
+    draw_blue_players();
+    draw_goal();
+    draw_ball();
+
+    draw_skybox();
+
+}
+
+void MainController::draw_reflectors() {
+    //  Model
+    auto resources = get<engine::resources::ResourcesController>();
+    auto graphics = get<engine::graphics::GraphicsController>();
+
+    engine::resources::Model *reflector = resources->model("reflector");
+    //  Shader
+    engine::resources::Shader *shader = resources->shader("basic");
+
+    shader->use();
+
+    shader->set_mat4("projection", graphics->projection_matrix());
+    shader->set_mat4("view", graphics->camera()->view_matrix());
+
+    // Crtanje PRVOG reflektora
+    glm::mat4 model1 = glm::mat4(1.0f);// Resetujemo matricu
+
+    model1 = glm::translate(model1, glm::vec3(ref_x, ref_y, ref_z));
+    model1 = glm::rotate(model1, glm::radians(ref1_ugao), glm::vec3(ref_xv, ref_yv, ref_zv));
+    model1 = glm::scale(model1, glm::vec3(ref_skalirano));
+    shader->set_mat4("model", model1);
+    reflector->draw(shader);
+
+    // Crtanje DRUGOG reflektora
+    glm::mat4 model2 = glm::mat4(1.0f);// Resetujemo matricu
+    model2 = glm::translate(model2, glm::vec3(ref2_x, ref2_y, ref2_z));
+    model2 = glm::rotate(model2, glm::radians(ref2_ugao), glm::vec3(ref2_xv, ref2_yv, ref2_zv));
+    model2 = glm::scale(model2, glm::vec3(ref_skalirano));
+    shader->set_mat4("model", model2);
+    reflector->draw(shader);
+}
+
+void MainController::draw_red_players() {
+    //  Model
+    auto resources = get<engine::resources::ResourcesController>();
+    auto graphics = get<engine::graphics::GraphicsController>();
+
+    engine::resources::Model *red_player = resources->model("crveni");
+    //  Shader
+    engine::resources::Shader *shader = resources->shader("basic");
+
+    shader->use();
+
+    shader->set_mat4("projection", graphics->projection_matrix());
+    shader->set_mat4("view", graphics->camera()->view_matrix());
+
+    glm::mat4 model[2];
+
+    for (int i = 0; i < 2; i++) {
+        model[i] = glm::mat4(1.0f);
+        // namesti y
+        model[i] = glm::translate(model[i], glm::vec3(crveni_x - i * 7.0f, crveni_y, crveni_z));
+        model[i] = glm::rotate(model[i], glm::radians(ugao_crveni), glm::vec3(crveni_xv, crveni_yv, crveni_zv));
+        model[i] = glm::scale(model[i], glm::vec3(crveni_skalirano));
+        shader->set_mat4("model", model[i]);
+        red_player->draw(shader);
+    }
+
+};
+
+void MainController::draw_goalkeeper() {
+    //  Model
+    auto resources = get<engine::resources::ResourcesController>();
+    auto graphics = get<engine::graphics::GraphicsController>();
+
+    engine::resources::Model *goalkeeper = resources->model("goalkeeper");
+    //  Shader
+    engine::resources::Shader *shader = resources->shader("basic");
+
+    shader->use();
+
+    shader->set_mat4("projection", graphics->projection_matrix());
+    shader->set_mat4("view", graphics->camera()->view_matrix());
+
+    glm::mat4 model = glm::mat4(1.0f);
+    // namesti y
+    model = glm::translate(model, glm::vec3(golman_x, golman_y, golman_z));
+    model = glm::rotate(model, glm::radians(ugao_golman), glm::vec3(golman_xv, golman_yv, golman_zv));
+    model = glm::scale(model, glm::vec3(golman_skalirano));
+    shader->set_mat4("model", model);
+    goalkeeper->draw(shader);
+
+};
+
+void MainController::draw_blue_players() {
+    //  Model
+    auto resources = get<engine::resources::ResourcesController>();
+    auto graphics = get<engine::graphics::GraphicsController>();
+
+    engine::resources::Model *blue_player = resources->model("plavi");
+    //  Shader
+    engine::resources::Shader *shader = resources->shader("basic");
+
+    shader->use();
+
+    shader->set_mat4("projection", graphics->projection_matrix());
+    shader->set_mat4("view", graphics->camera()->view_matrix());
+
+    glm::mat4 model[4];
+
+    for (int i = 0; i < 4; i++) {
+        model[i] = glm::mat4(1.0f);
+        // namesti y
+        model[i] = glm::translate(model[i], glm::vec3(plavi_x + 0.65f * i, plavi_y, plavi_z));
+        model[i] = glm::rotate(model[i], glm::radians(ugao_plavi), glm::vec3(plavi_xv, plavi_yv, plavi_zv));
+        model[i] = glm::scale(model[i], glm::vec3(plavi_skalirano));
+        shader->set_mat4("model", model[i]);
+        blue_player->draw(shader);
+    }
+
+
+};
 
 void MainController::update() { update_camera(); }
 
