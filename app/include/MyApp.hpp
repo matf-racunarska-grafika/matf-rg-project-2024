4d14679eb3b7e0289712d99fd1d46fffaaab2792
filipn <filipndc@gmail.com>
2025-09-01 22:24:05 +0200

    Project Inicialization

---
+++ app/include/MyApp.hpp
@@ -0,0 +1,17 @@
+//
+// Created by filipn on 9/1/25.
+//
+
+#ifndef MYAPP_HPP
+#define MYAPP_HPP
+#include <engine/core/App.hpp>
+
+namespace app {
+
+class MyApp : public engine::core::App {
+    void app_setup() override;
+};
+
+}// app
+
+#endif //MYAPP_HPP
---
+++ app/src/MyApp.cpp
@@ -0,0 +1,11 @@
+//
+// Created by filipn on 9/1/25.
+//
+
+#include <MyApp.hpp>
+
+#include <spdlog/spdlog.h>
+
+namespace app {
+void MyApp::app_setup() { spdlog::info("App setup completed"); }
+}// app
