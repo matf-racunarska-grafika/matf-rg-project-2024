f1e1a8c2054a13555e9c571c0fc9310e2fdabb47
filipn <filipndc@gmail.com>
2025-09-01 22:59:37 +0200

    Create base for MainController

---
+++ app/include/MainController.hpp
@@ -0,0 +1,16 @@
+//
+// Created by filipn on 9/1/25.
+//
+
+#ifndef MAINCONTROLER_HPP
+#define MAINCONTROLER_HPP
+
+
+
+class MainControler {
+
+};
+
+
+
+#endif //MAINCONTROLER_HPP
---
+++ app/src/MainController.cpp
@@ -0,0 +1,5 @@
+//
+// Created by filipn on 9/1/25.
+//
+
+#include "../include/MainControler.hpp"
