15c2a02755d97e8acec62c76de92f06ae5597b38
Bogdan Micic <bogdanmicic03vr@gmail.com>
2025-09-14 17:11:13 +0200

    shader adjusted to be used for instanced drawing

--- app/resources/shaders/tree.glsl
+++ app/resources/shaders/tree.glsl
@@ -4,8 +4,8 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoord;
+layout (location = 3) in mat4 aModel;
 
-uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
@@ -14,7 +14,7 @@ out vec3 normal;
 out vec2 texCoord;
 
 void main() {
-    vec4 worldPos = model * vec4(aPos, 1.0f);
+    vec4 worldPos = aModel * vec4(aPos, 1.0f);
     fragPos = worldPos.xyz;
     normal = aNormal;
     texCoord = aTexCoord;
