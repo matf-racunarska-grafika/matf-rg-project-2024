2a3b1bc786283baf17a13b522a3e945eae0613f8
nikola <nikola.trifunovic14@gmail.com>
2025-10-08 21:29:27 +0200

    MyController: mouse and keyboard events off while GUI is enabled

--- app/src/MyController.cpp
+++ app/src/MyController.cpp
@@ -4,6 +4,7 @@
 
 #include "../include/MyController.hpp"
 
+#include "GUIController.hpp"
 #include "engine/graphics/GraphicsController.hpp"
 #include "engine/graphics/OpenGL.hpp"
 #include "engine/platform/PlatformController.hpp"
@@ -17,8 +18,11 @@ public:
 };
 
 void MainPlatformEventObserver::on_mouse_move(engine::platform::MousePosition position) {
-    auto camera = engine::core::Controller::get<engine::graphics::GraphicsController>()->camera();
-    camera->rotate_camera(position.dx, position.dy);
+    auto gui = engine::core::Controller::get<GUIController>();
+    if (!gui->is_enabled()) {
+        auto camera = engine::core::Controller::get<engine::graphics::GraphicsController>()->camera();
+        camera->rotate_camera(position.dx, position.dy);
+    }
 }
 
 
@@ -48,21 +52,24 @@ void MyController::draw_backpack() {
     model->draw(shader);
 }
 void MyController::update_camera() {
-    auto platform = get<engine::platform::PlatformController>();
-    auto graphics = get<engine::graphics::GraphicsController>();
-    auto camera = graphics->camera();
-    auto dt = platform->dt();
-    if(platform->key(engine::platform::KeyId::KEY_W).is_down()) {
-        camera->move_camera(engine::graphics::Camera::Movement::FORWARD, dt);
-    }
-    if(platform->key(engine::platform::KeyId::KEY_A).is_down()) {
-        camera->move_camera(engine::graphics::Camera::Movement::LEFT, dt);
-    }
-    if(platform->key(engine::platform::KeyId::KEY_S).is_down()) {
-        camera->move_camera(engine::graphics::Camera::Movement::BACKWARD, dt);
-    }
-    if(platform->key(engine::platform::KeyId::KEY_D).is_down()) {
-        camera->move_camera(engine::graphics::Camera::Movement::RIGHT, dt);
+    auto gui_controller = get<GUIController>();
+    if (!gui_controller->is_enabled()) {
+        auto platform = get<engine::platform::PlatformController>();
+        auto graphics = get<engine::graphics::GraphicsController>();
+        auto camera = graphics->camera();
+        auto dt = platform->dt();
+        if (platform->key(engine::platform::KeyId::KEY_W).is_down()) {
+            camera->move_camera(engine::graphics::Camera::Movement::FORWARD, dt);
+        }
+        if (platform->key(engine::platform::KeyId::KEY_A).is_down()) {
+            camera->move_camera(engine::graphics::Camera::Movement::LEFT, dt);
+        }
+        if (platform->key(engine::platform::KeyId::KEY_S).is_down()) {
+            camera->move_camera(engine::graphics::Camera::Movement::BACKWARD, dt);
+        }
+        if (platform->key(engine::platform::KeyId::KEY_D).is_down()) {
+            camera->move_camera(engine::graphics::Camera::Movement::RIGHT, dt);
+        }
     }
 }
 void MyController::update() {
