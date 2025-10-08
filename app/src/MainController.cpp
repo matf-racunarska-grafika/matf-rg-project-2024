de9b76f7793b0d70e27dd8981e7c49b5c63493e5
Ognjen Marković <ognjen.mark03@gmail.com>
2025-10-08 21:44:01 +0200

    Change skybox (problem with top image)

--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -164,7 +164,7 @@ namespace app {
 
     void MainController::draw_skybox() {
         auto resources = engine::core::Controller::get<engine::resources::ResourcesController>();
-        auto skybox = resources->skybox("mountain_skybox");
+        auto skybox = resources->skybox("used_skybox");
         auto shader = resources->shader("skybox");
         auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
         graphics->draw_skybox(shader, skybox);
