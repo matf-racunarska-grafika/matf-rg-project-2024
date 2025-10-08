a9f5eec7e6aae33e961d2d7787d4ff3dc98d168b
bojana <bojanaz0711@gmail..com>
2025-10-08 20:40:18 +0200

    Added GUI controller

--- app/src/MyApp.cpp
+++ app/src/MyApp.cpp
@@ -4,12 +4,15 @@
 
 #include <MyApp.hpp>
 #include <MyController.hpp>
+#include <GUIController.hpp>
 #include <engine/core/Controller.hpp>
 #include <spdlog/spdlog.h>
 namespace app {
 void MyApp::app_setup() {
     spdlog::info("App setup");
     auto main_controller = register_controller<MyController>();
+    auto gui_controller = register_controller<GUIController>();
     main_controller->after(engine::core::Controller::get<engine::core::EngineControllersEnd>());
+    main_controller->after(main_controller);
 }
 }
