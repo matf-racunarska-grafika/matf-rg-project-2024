6c0dcc31311b8f41b2186320fe071407c2e3bac5
zesla <zeljkozekavicic@gmail.com>
2025-10-09 19:30:11 +0200

    Some init classes added.

--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -1,8 +1,11 @@
+#include <MyApp.hpp>
 #include <engine/core/Engine.hpp>
 
 /**
  * Start here...
  */
 int main(int argc, char** argv) {
+    auto app = std::make_unique<app::MyApp>();
+    return app->run(argc,argv);
     return 0;
 }
