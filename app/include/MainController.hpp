7df9988626fa0a862a0fe3257b43e25e09cbdc3e
zesla <zeljkozekavicic@gmail.com>
2025-10-10 03:03:42 +0200

    Bed shapeshifted a bit.

--- app/include/MainController.hpp
+++ app/include/MainController.hpp
@@ -10,13 +10,15 @@ namespace app{
 class MainController: public engine::core::Controller {
     void initialize() override;
     bool loop() override;
+    void draw_bed();
+    void begin_draw() override;
+    void draw() override;
+    void end_draw() override;
 
 public:
     std::string_view name() const override {
         return "app::MainController";
     }
-
-
 
 };
 
--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -3,22 +3,56 @@
 //
 
 #include <MainController.hpp>
+#include <engine/core/App.hpp>
+#include <engine/graphics/GraphicsController.hpp>
+#include <engine/graphics/OpenGL.hpp>
 #include <engine/platform/PlatformController.hpp>
 #include <spdlog/spdlog.h>
 
 #include <engine/platform/PlatformController.hpp>
+#include <engine/resources/ResourcesController.hpp>
 namespace app {
-
-
-    void app::MainController::initialize() {
-        spdlog::info("KOntroler inicijalizovan");
-    };
-    bool MainController::loop() {
-        auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
-        if (platform->key(engine::platform::KeyId::KEY_ESCAPE).is_down()) {
-            return false;
-        }
-        return true;
+void app::MainController::initialize() {
+    spdlog::info("KOntroler inicijalizovan");
+};
+bool MainController::loop() {
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+    if (platform->key(engine::platform::KeyId::KEY_ESCAPE).is_down()) {
+        return false;
     }
+    return true;
+}
+
+void MainController::draw_bed() {
+    auto resources = engine::core::Controller::get<engine::resources::ResourcesController>();
+    auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+    engine::resources::Model* bed = resources->model("bed");
+    engine::resources::Shader* shader = resources->shader("basic");
+    bed->draw(shader);
+
+    shader->use();
+    shader->set_mat4("projection", graphics->projection_matrix());
+    shader->set_mat4("view", graphics->camera()->view_matrix());
+    glm::mat4 model = glm::mat4(1.0);
+    model = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
+    model = glm::scale(model, glm::vec3(0.3f));
+    model = glm::rotate(model, 45.0f, glm::vec3(0.0f,3.0f,0.0f));
+    shader->set_mat4("model", model);
+        bed->draw(shader);
+}
+
+void MainController::begin_draw() {
+engine::graphics::OpenGL::clear_buffers();
+}
+
+void MainController::draw() {
+    draw_bed();
+}
+
+void MainController::end_draw() {
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+    platform->swap_buffers();
+    }
+
 
     }// namespace app;
