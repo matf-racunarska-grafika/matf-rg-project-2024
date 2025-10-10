5c1804c7e4d5eeb7f885a510aa240c92fab9c815
filipn <filipndc@gmail.com>
2025-10-10 21:34:48 +0200

    Added point_light_shadows shader

---
+++ app/resources/shaders/point_light_shadows.glsl
@@ -0,0 +1,37 @@
+//#shader vertex
+#version 330 core
+layout (location = 0) in vec3 aPos;
+
+uniform mat4 model;
+
+void main()
+{
+    gl_Position = model * vec4(aPos, 1.0);
+}
+
+//#shader geometry
+#version 330 core
+layout (location = 0) in vec3 aPos;
+
+uniform mat4 model;
+
+void main()
+{
+    gl_Position = model * vec4(aPos, 1.0);
+}
+
+//#shader fragment
+#version 330 core
+in vec4 FragPos;
+
+uniform vec3 lightPos;
+uniform float far_plane;
+
+void main()
+{
+    float lightDistance = length(FragPos.xyz - lightPos);
+
+    lightDistance = lightDistance / far_plane;
+
+    gl_FragDepth = lightDistance;
+}
