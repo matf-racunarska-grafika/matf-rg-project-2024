1e2e801262bbdf76fd31a79642e3687f859fa297
MatijaDjordjevicc <116580132+MatijaDjordjevicc@users.noreply.github.com>
2025-10-11 07:14:55 +0200

    Close app on ESC

--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -6,11 +6,22 @@
 
 #include "spdlog/spdlog.h"
 
+#include <engine/platform/PlatformController.hpp>
+#include <engine/platform/PlatformEventObserver.hpp>
+
 namespace app {
 
-    void MainController::initialize() {
-        spdlog::info("MainController initialized");
+void MainController::initialize() {
+    spdlog::info("MainController initialized");
+}
+
+bool MainController::loop() {
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+    if (platform->key(engine::platform::KeyId::KEY_ESCAPE).is_down()) {
+        return false;
     }
+    return true;
+}
 
 
-} // app
+}// namespace app
