d44d20bd683e71f893724477af7ea4aed1ef2fc1
MatijaDjordjevicc <116580132+MatijaDjordjevicc@users.noreply.github.com>
2025-10-11 03:21:54 +0200

    Create empty MyApp and app_setup

---
+++ app/src/MyApp.cpp
@@ -0,0 +1,14 @@
+//
+// Created by matfrg on 10/10/25.
+//
+
+#include "MyApp.hpp"
+
+#include "spdlog/spdlog.h"
+
+namespace app {
+    void MyApp::app_setup() {
+        spdlog::info("App setup completed");
+    }
+
+} // app
