e2e04496a3f91b48c7c1fca2bd666b611e7b189b
dzonimatf03 <nikola.jovanovic030902@gmail.com>
2025-10-11 16:53:59 +0200

    Created black window.

---
+++ app/MyApp.cpp
@@ -0,0 +1,12 @@
+//
+// Created by root on 10/11/25.
+//
+
+#include "MyApp.hpp"
+
+#include <spdlog/spdlog.h>
+
+namespace app {
+void MyApp::app_setup() { spdlog::info("App setup complited"); }
+
+}// app
---
+++ app/MyApp.hpp
@@ -0,0 +1,15 @@
+//
+// Created by root on 10/11/25.
+//
+
+#ifndef MATF_RG_PROJECT_MYAPP_HPP
+#define MATF_RG_PROJECT_MYAPP_HPP
+#include <engine/core/App.hpp>
+
+namespace app {
+class MyApp : public engine::core::App {
+    void app_setup() override;
+};
+}// app
+
+#endif //MATF_RG_PROJECT_MYAPP_HPP
