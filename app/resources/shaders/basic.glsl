e725df7b2dd18cbfce20112906e079361c41eff8
filipn <filipndc@gmail.com>
2025-10-11 13:36:58 +0200

    Change spotlight calculation

--- app/resources/shaders/basic.glsl
+++ app/resources/shaders/basic.glsl
@@ -96,7 +96,7 @@ void main() {
     vec3 result = CalcPointLight(moon, normal, FragPos, viewDir);
     result += CalcPointLight(sun, normal, FragPos, viewDir);
 
-    if (sun.position.y < 1.5) {
+    if (sun.position.y < 2.5) {
         for (int i = 0; i < NUM_OF_LAMPS; i++) {
             result += CalcSpotLight(lamps[i], normal, FragPos, viewDir);
         }
