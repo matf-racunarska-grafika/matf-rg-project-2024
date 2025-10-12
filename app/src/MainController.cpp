a53d31575347939ddb00669d559caaf418f3fd0c
MatijaDjordjevicc <116580132+MatijaDjordjevicc@users.noreply.github.com>
2025-10-12 08:37:50 +0200

    Dodato kretanje kamere WASD

--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -45,6 +45,32 @@ void MainController::draw_island() {
     island->draw(shader);
 
 }
+
+void MainController::update_camera() {
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+    auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+    auto camera = graphics->camera();
+    float dt = platform->dt();
+    if(platform->key(engine::platform::KeyId::KEY_W).is_down()) {
+        camera->move_camera(engine::graphics::Camera::Movement::FORWARD,dt);
+    }
+    if(platform->key(engine::platform::KeyId::KEY_A).is_down()) {
+        camera->move_camera(engine::graphics::Camera::Movement::LEFT,dt);
+    }
+    if(platform->key(engine::platform::KeyId::KEY_S).is_down()) {
+        camera->move_camera(engine::graphics::Camera::Movement::BACKWARD,dt);
+    }
+    if(platform->key(engine::platform::KeyId::KEY_D).is_down()) {
+        camera->move_camera(engine::graphics::Camera::Movement::RIGHT,dt);
+    }
+}
+
+
+void MainController::update() {
+    update_camera();
+}
+
+
 void MainController::begin_draw() {
     engine::graphics::OpenGL::clear_buffers();
 }
