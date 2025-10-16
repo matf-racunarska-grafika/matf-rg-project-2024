f6f9a5483e7541d89504b5069bdd62d94bfafdcc
MatijaDjordjevicc <116580132+MatijaDjordjevicc@users.noreply.github.com>
2025-10-16 08:07:43 +0200

    uklonjene ne koriscene promenljice i uniformi

--- app/resources/shaders/zvezda.glsl
+++ app/resources/shaders/zvezda.glsl
@@ -2,11 +2,7 @@
 #version 330 core
 
 layout (location=0) in vec3 aPos;
-layout (location=1) in vec3 aNormal;
-layout (location=2) in vec2 aTexCoords;
 
-out vec2 TexCoords;
-out vec3 Normal;
 out vec3 FragPos;
 
 uniform mat4 model;
@@ -16,9 +12,6 @@ uniform mat4 projection;
 void main(){
 
     FragPos = vec3(model * vec4(aPos,1.0));
-    Normal = aNormal;
-    TexCoords = aTexCoords;
-
     gl_Position = projection * view * vec4(FragPos,1.0);
 }
 
@@ -27,9 +20,6 @@ void main(){
 #version 330 core
 
 out vec4 FragColor;
-in vec2 TexCoords;
-
-uniform sampler2D texture_diffuse1;
 
 void main(){
     FragColor=vec4(0.992,0.875,0.49,1.0f);
