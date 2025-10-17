ca22ee411e2bbf3e068edfd4bb5c42b5e5778da4
duki <duki@localhost>
2025-10-18 00:04:21 +0200

    Added working example code

--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -1,8 +1,16 @@
 #include <engine/core/Engine.hpp>
+#include <memory>
+#include <spdlog/spdlog.h>
 
-/**
- * Start here...
- */
-int main(int argc, char** argv) {
-    return 0;
+class MyApp final : public engine::core::App {
+protected:
+    void app_setup() override {
+        spdlog::info("\x1b[31mWorking example\x1b[0m");
+    }
+};
+
+
+int main(int argc, char **argv) {
+    auto app = std::make_unique<MyApp>();
+    return app->run(argc, argv);
 }
