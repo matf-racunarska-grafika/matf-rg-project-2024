0b2ca4c43e37708951cc70be12a9420b41e083a9
MatijaDjordjevicc <116580132+MatijaDjordjevicc@users.noreply.github.com>
2025-10-21 14:07:21 +0200

    Dodan BrightColor output u standard_lighting shaderu za bloom

--- app/resources/shaders/standard_lighting.glsl
+++ app/resources/shaders/standard_lighting.glsl
@@ -31,7 +31,8 @@ in vec3 FragPos;
 in vec3 Normal;
 in vec2 TexCoords;
 
-out vec4 FragColor;
+layout (location = 0) out vec4 FragColor;
+layout (location = 1) out vec4 BrightColor;
 
 struct Material{
     sampler2D diffuse;
@@ -82,6 +83,15 @@ void main(){
     }
 
     FragColor = vec4(result,1.0);
+
+    //bloom deo
+    float brightness = dot(result,vec3(0.2126,0.7152,0.0722));
+    if(brightness>1.0){
+        BrightColor = vec4(result,1.0);
+    }else{
+        BrightColor = vec4(0.0,0.0,0.0,1.0);
+    }
+
 }
 
 vec3 CalcDirLight(DirLight light, vec3 normal,vec3 viewDir){
