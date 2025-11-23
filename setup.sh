ec2551b32f5fee0f268e471281c69f3a6870ce9b
Marko Spasic <spaskeasm@gmail.com>
2025-11-23 14:12:15 +0100

    Use assimp installed on the system (#65)

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo add-apt-repository universe ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt update
-sudo apt install pkg-config g++-13 clang-format clang-tidy cmake git build-essential libwayland-dev libxkbcommon-dev xorg-dev libgl1-mesa-dev mesa-common-dev mesa-utils doxygen graphviz
+sudo apt install pkg-config g++-13 clang-format clang-tidy cmake git build-essential libwayland-dev libxkbcommon-dev xorg-dev libgl1-mesa-dev mesa-common-dev mesa-utils doxygen graphviz libassimp-dev
 
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 10
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-13 10
@@ -15,5 +15,3 @@ sudo apt install python3-pip python3-venv -y
 
 python3 -m venv .venv
 source ".venv/bin/activate" && pip3 install libclang
-
-glxinfo | grep OpenGL
